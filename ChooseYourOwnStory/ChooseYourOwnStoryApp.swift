802b976625e53c2e844e3ca9eee06190978e8a9d
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-26 17:54:56 -0400

    Initial Commit

---
+++ ChooseYourOwnStory/ChooseYourOwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnStoryApp.swift
+//  ChooseYourOwnStory
+//
+//  Created by Gabriel Marquez on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourOwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
